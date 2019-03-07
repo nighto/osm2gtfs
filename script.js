716283423e9b213996d262dd22b0bca96b77070a
Arlindo Pereira <arlindo.pereira@mprj.mp.br>
2019-03-07 02:43:44 -0300

    fetch descendent data automatically + debug

--- script.js
+++ script.js
@@ -5,10 +5,40 @@ let OSMData = {
 
 // Some constants
 const OSM_API = 'https://www.openstreetmap.org/api/0.6'
+const DEBUG = true
+
+const debugOSMData = () => {
+    let debug = ``
+    if (OSMData.routeMasters && OSMData.routeMasters.length > 0) {
+        OSMData.routeMasters.forEach(routeMaster => {
+            if (routeMaster.id) {
+                debug += `- Route Master: id ${routeMaster.id}\n`
+                debug += `  Name: ${routeMaster.tags.filter(tag => tag.k === 'name')[0].v}\n`
+                routeMaster.members.forEach(route => {
+                    if (route.data && route.data.id) {
+                        debug += `\t- Route: id ${route.data.id}\n`
+                        debug += `\t  Name: ${route.data.tags.filter(tag => tag.k === 'name')[0].v}\n`
+                        let stopsWithData = route.data.members.filter(node => node.role === 'stop' && node.data).length
+                        debug += `\t\t- Stops with data: ${stopsWithData}\n`
+                        let stopsWithoutData = route.data.members.filter(node => node.role === 'stop' && !node.data).length
+                        debug += `\t\t- Stops without data: ${stopsWithoutData}\n`
+                        let waysWithData = route.data.members.filter(way => way.type === 'way' && way.data && way.data.nds.filter(nd => nd.data)).length
+                        debug += `\t\t- Ways with data: ${waysWithData}\n`
+                        let waysWithoutData = route.data.members.filter(way => way.type === 'way' && (!way.data || way.data.nds.filter(nd => nd.data).length === 0)).length
+                        debug += `\t\t- Ways without data: ${waysWithoutData}\n`
+                    }
+                })
+            }
+        })
+    } else {
+        debug = 'Not yet initialized.'
+    }
+    document.querySelector('pre').textContent = debug
+}
 
 // Fetch OSM data for a route master id
 const fetchRouteMaster = async id => {
-    routeMasterData = await fetchOSMData('relation', id)
+    let routeMasterData = await fetchOSMData('relation', id)
     OSMData.routeMasters.push(routeMasterData)
     console.log(OSMData)
 }
@@ -19,11 +49,11 @@ const fetchOSMData = async (type, id) => {
     console.log('Fetching ' + url)
     let response = await fetch(url)
     let data = await response.text()
-    return parseOSMData(type, data)
+    return await parseOSMData(type, data)
 }
 
 // Parse data from OSM API
-const parseOSMData = (type, xmlText) => {
+const parseOSMData = async (type, xmlText) => {
     let parser = new DOMParser()
     let doc = parser.parseFromString(xmlText, 'application/xml')
     // reading directly node/way/relation inside of <osm> because root element doesn't matter.
@@ -38,10 +68,16 @@ const parseOSMData = (type, xmlText) => {
     let ndsList = readNodeDescendentsNamed(thisNode, 'nd')
     if (ndsList.length) {
         attributes.nds = ndsList
+        attributes.nds.forEach(async nd => {
+            nd.data = await fetchOSMData('node', nd.ref)
+        })
     }
     let membersList = readNodeDescendentsNamed(thisNode, 'member')
     if (membersList.length) {
         attributes.members = membersList
+        attributes.members.forEach(async member => {
+            member.data = await fetchOSMData(member.type, member.ref)
+        })
     }
     return attributes
 }
@@ -78,3 +114,8 @@ const readRouteMasters = () => {
 
 // Attach button event
 document.querySelector('#process').onclick = readRouteMasters
+
+// Fires debug
+if (DEBUG) {
+    setInterval(() => { debugOSMData() }, 100)
+}
